23374e6a4be2afe2cb72daa0fa64f94b2164f925
Kipras Melnikovas <kipras@kipras.org>
2022-12-01 08:50:18 +0200

    cputils: update js template - chmod +x, add last console.log, clear up missing stdin
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- .config/cputils/template.js
+++ .config/cputils/template.js
@@ -2,11 +2,11 @@
 const assert = require("assert")
 
 let s = require("fs").readFileSync(0).toString()
-assert(s.length, "stdin provided")
+assert(s.length, "stdin missing")
 
 s = s.split("\n")<++>
 
 <++>
 
-
+console.log(s)
 

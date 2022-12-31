5b4d478f0b208286b9e12cf8e9fcba4d39c16564
Kipras Melnikovas <kipras@kipras.org>
2022-12-31 04:44:59 +0200

    cputils: clean up default template
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- .config/cputils/template.old.cpp
+++ .config/cputils/template.old.cpp
@@ -1,4 +1,5 @@
 #pragma  GCC optimize     ("O3")
+#pragma  GCC target     ("sse4")
 
 #if (0 || defined(DEBUG)) && !defined(EVAL)
 	#include "debug.h" // https://github.com/kiprasmel/debug.h
@@ -23,6 +24,9 @@ constexpr int MOD   =  1e9  + 7;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+
+	// const char* __fin  = "in" ; freopen(__fin , "r", stdin ); assert(std::ifstream(__fin).good());
+	// const char* __fout = "out"; freopen(__fout, "w", stdout);
 
 	<++>
 
